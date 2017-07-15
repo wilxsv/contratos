432fedf437fc7ff54aa85e06cea758246ed42891
frederick <frederick@fjcode>
2017-07-15 17:01:55 -0600

    orar para que no se arruine

--- src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
+++ src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
@@ -7,4 +7,8 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class InicioProcesoController extends Controller
 {
+	public function inicioAction()
+	{
+		return $this->render('MinsalPlantillaBundle:InicioProceso:inicio.html.twig');
+	}
 }
