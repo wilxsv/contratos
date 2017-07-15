4cbe76464acece4cf9876c6363c3c33978f209b6
frederick <frederick@fjcode>
2017-07-15 15:59:32 -0600

    estructura de archivos de analizador

---
+++ src/Minsal/ContratoBundle/Controller/AnalizadorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Minsal\ContratoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+/*
+	 * Direccion Fisica: src/Minsal/ContratoBundle/Controller/AnalizadorController.php
+
+*/
+
+class AnalizadorController extends Controller
+{	
+	/*
+		* funcion para el renderizado de la dashboard del analisis, recibe como parametro
+		el codigo de licitacion
+	*/
+	public function dashboardAction()
+	{
+		return $this->render('MinsalPlantillaBundle:Analizador:dashboard.html.twig');
+	}
+
+}
---
+++ src/Minsal/PlantillaBundle/Resources/views/Analizador/dashboard.html.twig
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hola mundo</title>
+</head>
+<body>
+
+</body>
+</html>
