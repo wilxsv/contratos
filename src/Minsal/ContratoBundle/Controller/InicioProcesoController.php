01844055f3876bd522f645a478f4ba0ab355fc8c
frederick <frederick@fjcode>
2017-07-15 20:13:32 -0600

    se termino el inicio de incremento solo fata el array de datos

--- src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
+++ src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
@@ -8,7 +8,15 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class InicioProcesoController extends Controller
 {
 	public function inicioAction()
-	{
-		return $this->render('MinsalPlantillaBundle:InicioProceso:inicio.html.twig');
+	{	
+		$codigosLicitaciones = array(
+			'LP 02/2017',
+			'LP 12/2017',
+			'LP 43/2017',
+			'LP 05/2017',
+			);
+		return $this->render('MinsalPlantillaBundle:InicioProceso:inicio.html.twig',array(
+			'codigosLicitaciones' => $codigosLicitaciones
+			));
 	}
 }
