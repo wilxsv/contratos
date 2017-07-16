86108b07d695af4b7c95fae4e23b215a63aee0e8
frederick <frederick@fjcode>
2017-07-16 00:51:02 -0600

    error en persistir los datos

--- src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
+++ src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
@@ -6,6 +6,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Minsal\ModeloBundle\Entity\ResumenIncremento;
+
+
 
 class InicioProcesoController extends Controller
 {
@@ -23,6 +26,39 @@ class InicioProcesoController extends Controller
 	}
 	public function crearIncrementoAction(Request $request)
 	{
+		$codigoLicitacion = $request->get('cod');
+		$Incremento = new ResumenIncremento();
+		$Incremento->setCodigoCompra($codigoLicitacion);
+		#aqui tiene que ir el jalado por las bases de datos
 		
+		$array = array(
+			"0" => array(
+					"num_contrato" =>"12/2017",
+					"cod_proveedor"=>"prov123",
+					"is_valid"=>1,
+					"cod_medicamento"=> "ibu123",
+					"medicamento_valid"=>0,
+					"precioUnitario"=>12.23,
+					"cantidad_comprada"=>12
+				),
+			"1" => array(
+					"num_contrato" =>"12/2017",
+					"cod_proveedor"=>"prov123",
+					"is_valid"=>1,
+					"cod_medicamento"=> "ibu123",
+					"medicamento_valid"=>0,
+					"precioUnitario"=>12.23,
+					"cantidad_comprada"=>12
+				)
+			);
+		$contratosJSON = json_encode($array);
+		$Incremento->setContratos($contratosJSON);
+		$Incremento->setEstado(1);
+		$Incremento->setFechaCreacion('2017-02-12');
+		$em = $this->getDoctrine()->getManager();
+        $em->persist($Incremento);
+        $em->flush();
+
+		return $this->redirectToRoute('minsal_contrato_inicio_proceso_inicio');
 	}
 }
