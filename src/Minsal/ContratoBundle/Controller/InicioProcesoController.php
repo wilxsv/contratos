3ef8aede3aa4a050ecac46f113b41d2d2e6ecfa8
frederick <frederick@fjcode>
2017-07-21 02:08:00 -0600

    reinicio de entidades

--- src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
+++ src/Minsal/ContratoBundle/Controller/InicioProcesoController.php
@@ -14,16 +14,14 @@ class InicioProcesoController extends Controller
 {
 	public function inicioAction()
 	{	
-		$em = $this->getDoctrine()->getManager();
-		$licitaciones = $em->getRepository('MinsalModeloBundle:Licitacion')->findAll();
-		
-		$em = $this->getDoctrine()->getManager();
-        $resumen = $em->getRepository('MinsalModeloBundle:ProcesoIncremento')->findAll();
 
-		return $this->render('MinsalPlantillaBundle:InicioProceso:inicio.html.twig',array(
-			'codigosLicitaciones' => $licitaciones,
-			'incrementos' => $resumen
-			));
+		$service_url = 'http://192.168.1.2:8080/v1/sinab/procesoscompras?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3';
+        $curl = curl_init($service_url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl_response = curl_exec($curl);
+        curl_close($curl);
+        $respuesta = json_decode($curl_response,true);
+		return $this->render('MinsalPlantillaBundle:InicioProceso:inicio.html.twig');
 	}
 
 	public function crearIncrementoAction(Request $request)
