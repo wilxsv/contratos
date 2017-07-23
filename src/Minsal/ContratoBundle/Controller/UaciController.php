9506c3e49a4ff0d5c94f8a8b9146bf1ba7b92a38
frederick <frederick@fjcode>
2017-07-23 05:41:40 -0600

    Solucion de proveedores

--- src/Minsal/ContratoBundle/Controller/UaciController.php
+++ src/Minsal/ContratoBundle/Controller/UaciController.php
@@ -26,7 +26,7 @@ class UaciController extends Controller
 	public function proveedorUaciAction($cod)
 	{
 
-		$sql = "SELECT co.numero_contrato, p.nombre_proveedor FROM ctl_modalidad_compra AS c JOIN ctl_contrato AS co ON co.numero_modalidad_compra = c.id JOIN ctl_proveedor AS p ON co.contrato_proveedor=p.id WHERE c.numero_modalidad LIKE '$cod' ";
+		/*$sql = "SELECT co.numero_contrato, p.nombre_proveedor FROM ctl_modalidad_compra AS c JOIN ctl_contrato AS co ON co.numero_modalidad_compra = c.id JOIN ctl_proveedor AS p ON co.contrato_proveedor=p.id WHERE c.numero_modalidad LIKE 'DR-CAFTA N° 03/2015'";
 
 		$rsm = new ResultSetMapping();
 		$rsm->addEntityResult('MinsalModeloBundle:CtlContrato', 'co');
@@ -37,6 +37,24 @@ class UaciController extends Controller
 		$proveedores = $nq->getResult();
 
 		return $this->render('MinsalPlantillaBundle:Uaci:manejo_proveedores_uaci.html.twig', array(
+			'proveedores'=>$proveedores,
+			
+			));*/
+
+		$sql = "SELECT co.id_contrato,p.id,co.numero_contrato, p.nombre_proveedor FROM ctl_modalidad_compra AS c JOIN ctl_contrato AS co ON co.numero_modalidad_compra = c.id JOIN ctl_proveedor AS p ON co.contrato_proveedor=p.id WHERE c.numero_modalidad LIKE 'DR-CAFTA N° 03/2015'";
+			$rsm = new ResultSetMapping;
+			$rsm->addEntityResult('MinsalModeloBundle:CtlContrato', 'co');
+			$rsm->addEntityResult('MinsalModeloBundle:CtlProveedor', 'p');
+			$rsm->addEntityResult('MinsalModeloBundle:CtlModalidadCompra', 'c');
+			$rsm->addFieldResult('co','numero_contrato','numeroContrato');
+			$rsm->addFieldResult('co','id_contrato','idContrato');
+			$rsm->addFieldResult('p','nombre_proveedor','nombreProveedor');
+			$rsm->addFieldResult('p','id','id');
+
+			$nq = $this->getDoctrine()->getManager()->createNativeQuery($sql, $rsm);
+			$proveedores = $nq->getResult();
+
+			return $this->render('MinsalPlantillaBundle:Uaci:manejo_proveedores_uaci.html.twig', array(
 			'proveedores'=>$proveedores,
 			
 			));
