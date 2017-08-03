d829cf9c98a4f00f4f4f40eb9cbb756182b6557b
frederick <frederick@fjcode>
2017-08-02 21:34:51 -0600

    analizador

--- src/Minsal/ContratoBundle/Controller/AnalizadorController.php
+++ src/Minsal/ContratoBundle/Controller/AnalizadorController.php
@@ -22,93 +22,111 @@ class AnalizadorController extends Controller
 		* funcion para el renderizado de la dashboard del analisis, recibe como parametro
 		el codigo de licitacion
 	*/
-	public function dashboardAction($incremento,$contrato)
-	{
-		/* parte del analizador presupuestario*/
-		$em = $this->getDoctrine()->getManager();
-		$productocontrato = $em->getRepository('MinsalModeloBundle:MtnProductoContrato')->findBy(array(
-				'mtnContrato' => $contrato
-			));
-		$incre = $em->getRepository('MinsalModeloBundle:CtlIncremento')->findOneBy(array(
-				'id' => $incremento
-			));
-		$contrat = $em->getRepository('MinsalModeloBundle:CtlContrato')->findOneBy(array(
-				'idContrato' => $contrato
-			));
-		/*aqui se obtiene la cantidad incrementada actualmente por contrato*/
-		$qb = $em->createQueryBuilder();
-		$qb ->select('SUM(e.montoIncrementar)');
-		$qb->from('MinsalModeloBundle:CtlContratosIncrementos','e');
-		$qb->where('e.idContrato = :contrato');
-		$qb->setParameter('contrato',$contrato);
-		$count = $qb->getQuery()->getSingleScalarResult();
+	// public function dashboardAction($incremento,$contrato)
+	// {
+	// 	/* parte del analizador presupuestario*/
+	// 	$em = $this->getDoctrine()->getManager();
+	// 	$productocontrato = $em->getRepository('MinsalModeloBundle:MtnProductoContrato')->findBy(array(
+	// 			'mtnContrato' => $contrato
+	// 		));
+	// 	$incre = $em->getRepository('MinsalModeloBundle:CtlIncremento')->findOneBy(array(
+	// 			'id' => $incremento
+	// 		));
+	// 	$contrat = $em->getRepository('MinsalModeloBundle:CtlContrato')->findOneBy(array(
+	// 			'idContrato' => $contrato
+	// 		));
+	// 	/*aqui se obtiene la cantidad incrementada actualmente por contrato*/
+	// 	$qb = $em->createQueryBuilder();
+	// 	$qb ->select('SUM(e.montoIncrementar)');
+	// 	$qb->from('MinsalModeloBundle:CtlContratosIncrementos','e');
+	// 	$qb->where('e.idContrato = :contrato');
+	// 	$qb->setParameter('contrato',$contrato);
+	// 	$count = $qb->getQuery()->getSingleScalarResult();
 		
-		/* analisis de criticidad*/
-		$estimacion = $incre->getEstimacion();
-		$programacion = 1;
-		$em = $this->getDoctrine()->getManager();
-		$re = $em->getRepository('MinsalModeloBundle:MtnMedicamentoIncremento')->findOneBy(array(
-			'incrementoid'=>$incremento,'contratoid'=>$contrato
-			));
-		$productos = json_decode($re->getMedicamentos(),true);
-		$listadoCoberturas = array();
+	// 	/* analisis de criticidad*/
+	// 	$estimacion = $incre->getEstimacion();
+	// 	$programacion = 1;
+	// 	$em = $this->getDoctrine()->getManager();
+	// 	$re = $em->getRepository('MinsalModeloBundle:MtnMedicamentoIncremento')->findOneBy(array(
+	// 		'incrementoid'=>$incremento,'contratoid'=>$contrato
+	// 		));
+	// 	$productos = json_decode($re->getMedicamentos(),true);
+	// 	$listadoCoberturas = array();
 
-		foreach ($productos['respuesta'] as $obj) {
-		    	$productoid = $obj["1"];
-		    	$service_url = 'http://192.168.1.4:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&programacion='.$programacion.'&idproducto='.$productoid;
+	// 	foreach ($productos['respuesta'] as $obj) {
+	// 	    	$productoid = $obj["1"];
+	// 	    	$service_url = 'http://192.168.1.4:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&programacion='.$programacion.'&idproducto='.$productoid;
 
-			    $curl = curl_init($service_url);
-			    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			    $curl_response = curl_exec($curl);
-			    curl_close($curl);
-			    $respuesta = json_decode($curl_response,true);
-		    	array_push($listadoCoberturas, $respuesta);
+	// 		    $curl = curl_init($service_url);
+	// 		    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	// 		    $curl_response = curl_exec($curl);
+	// 		    curl_close($curl);
+	// 		    $respuesta = json_decode($curl_response,true);
+	// 	    	array_push($listadoCoberturas, $respuesta);
 
-		    }
-	    return $this->render('MinsalPlantillaBundle:Analizador:dashboard.html.twig', array(
-			'productocontrato' => $productocontrato,'incre'=>$incre,'contrato'=>$contrat,
-			'saldoIncrementado' => $count,
-			'coberturas'=> $listadoCoberturas
-			));
-	}
+	// 	    }
+	//     return $this->render('MinsalPlantillaBundle:Analizador:dashboard.html.twig', array(
+	// 		'productocontrato' => $productocontrato,'incre'=>$incre,'contrato'=>$contrat,
+	// 		'saldoIncrementado' => $count,
+	// 		'coberturas'=> $listadoCoberturas
+	// 		));
+	// }
 
-	/*
-		* funcion que renderiza los contratos a incrementar
-	*/
+	// /*
+	// 	* funcion que renderiza los contratos a incrementar
+	// */
 
-	public function listadoAction($incremento)
-	{	
-		$em = $this->getDoctrine()->getManager();
-	    $contratos = $em->getRepository('MinsalModeloBundle:CtlContrato')->findByNumeroModalidadCompra($incremento);
+	// public function listadoAction($incremento)
+	// {	
+	// 	$em = $this->getDoctrine()->getManager();
+	//     $contratos = $em->getRepository('MinsalModeloBundle:CtlContrato')->findByNumeroModalidadCompra($incremento);
 
-	    $incremento = $em->getRepository('MinsalModeloBundle:CtlIncremento')->findOneBy(
-	    	array(
-	    		'incrementoModalidadCompra'=>$incremento
-	    		)
-	    	);
+	//     $incremento = $em->getRepository('MinsalModeloBundle:CtlIncremento')->findOneBy(
+	//     	array(
+	//     		'incrementoModalidadCompra'=>$incremento
+	//     		)
+	//     	);
 
 	   
-	    return $this->render('MinsalPlantillaBundle:Analizador:contratos.html.twig',array(
-	    	'contratos' => $contratos,
-	    	'incremento' => $incremento
-	    	));
-	}
+	//     return $this->render('MinsalPlantillaBundle:Analizador:contratos.html.twig',array(
+	//     	'contratos' => $contratos,
+	//     	'incremento' => $incremento
+	//     	));
+	// }
 
-	public function guardarIncrementoAction(Request $request)
-	{
-		$producto = $request->get('producto');
-        $cantidad = $request->get('cantidad');
-        $contrato = $request->get('contrato');
-        $monto = $request->get('monto');
-		$em = $this->getDoctrine()->getManager();
-		$nuevoContrIncre = new CtlContratosIncrementos();
-		$nuevoContrIncre->setIdProducto($producto)
-		->setCantidadIncrementar($cantidad)
-		->setIdContrato($contrato)
-		->setMontoIncrementar($monto);
-		 $em->persist($nuevoContrIncre);
-        $em->flush($nuevoContrIncre);
-        return new Response('exito');
-	}
+	// public function guardarIncrementoAction(Request $request)
+	// {
+	// 	$producto = $request->get('producto');
+ //        $cantidad = $request->get('cantidad');
+ //        $contrato = $request->get('contrato');
+ //        $monto = $request->get('monto');
+	// 	$em = $this->getDoctrine()->getManager();
+	// 	$nuevoContrIncre = new CtlContratosIncrementos();
+	// 	$nuevoContrIncre->setIdProducto($producto)
+	// 	->setCantidadIncrementar($cantidad)
+	// 	->setIdContrato($contrato)
+	// 	->setMontoIncrementar($monto);
+	// 	 $em->persist($nuevoContrIncre);
+ //        $em->flush($nuevoContrIncre);
+ //        return new Response('exito');
+	// }
+
+
+		public function listadoAction($incremento)
+		{
+			$em = $this->getDoctrine()->getManager();
+			$dql = "SELECT DISTINCT mc.id,mc.numeroModalidad,c.idContratoSibasi,c.numeroContrato,pr.codigoProveedor,pr.nombreProveedor,p.codigoProducto,p.nombreProducto,c.montoContrato,pc.cantidad,pc.precioUnitario FROM MinsalModeloBundle:CtlProducto p
+				INNER JOIN MinsalModeloBundle:MtnProductoContrato pc WITH p.idProductoSibasi=pc.mtnProducto 
+				INNER JOIN MinsalModeloBundle:CtlContrato c WITH pc.mtnContrato = c.idContratoSibasi INNER JOIN MinsalModeloBundle:CtlProveedor pr WITH pr.idProveedorSibasi = pc.mtnProveedor INNER JOIN MinsalModeloBundle:CtlModalidadCompra mc WITH mc.id=c.numeroModalidadCompra WHERE mc.id = $incremento AND pr.estadoProveedor=4 ORDER BY c.numeroContrato";
+				$listado = $em->createQuery($dql)->getResult();
+				return $this->render('MinsalPlantillaBundle:Analizador:contratos.html.twig',array(
+	     	'listado'=>$listado
+	     	));
+		}
 
 }
+
+
+
+
+
