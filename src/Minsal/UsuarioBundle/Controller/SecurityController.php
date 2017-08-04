2d94e6d6f32e30b3fd135e7b40f29b4f3bb6234d
Douglas Serbino <douglas@serbino>
2017-08-04 16:34:29 -0600

    Controlador de Seguridad

---
+++ src/Minsal/UsuarioBundle/Controller/SecurityController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Minsal\UsuarioBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class SecurityController extends Controller
+{
+}
---
+++ src/Minsal/UsuarioBundle/Tests/Controller/SecurityControllerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Minsal\UsuarioBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityControllerTest extends WebTestCase
+{
+}
