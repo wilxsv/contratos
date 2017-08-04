9a924e0ff9ac5acc994157b7373977226bcc29b1
Douglas Serbino <douglas@serbino>
2017-08-04 16:52:28 -0600

    Parte de los metodos y rutas necesarias

--- src/Minsal/UsuarioBundle/Controller/SecurityController.php
+++ src/Minsal/UsuarioBundle/Controller/SecurityController.php
@@ -7,4 +7,22 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class SecurityController extends Controller
 {
+	public function loginAction()
+	{
+		$authenticationUtils = $this->get('security.authentication_utils');
+
+		$error = $authenticationUtils->getLastAuthenticationError();
+
+		$lastUserName = $authenticationUtils->getLastUsername();
+
+		return $this->render('MinsalPlantillaBundle:Seguridad:login.html.twig', array(
+			'last_username' => $lastUserName, 
+			'error' => $error)
+		);
+	}
+
+	public function loginCheckAction()
+	{
+		
+	}
 }
