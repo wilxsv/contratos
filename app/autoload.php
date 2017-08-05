175a8a57b3f6699b323c1268422c9ecb0ff36229
Douglas Serbino <douglas@serbino>
2017-08-05 16:15:49 -0600

    Redireccion

--- app/autoload.php
+++ app/autoload.php
@@ -3,9 +3,12 @@
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
+
 /** @var ClassLoader $loader */
 $loader = require __DIR__.'/../vendor/autoload.php';
 $loader->add('', __DIR__.'/../src');
+
+
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -4,6 +4,47 @@ namespace AppBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Router;
+
 class AppBundle extends Bundle
 {
+	
+}
+
+
+
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+    protected $router, $security;
+    
+    public function __construct(Router $router, SecurityContext $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
+    
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+        
+        //$url = 'minsal_contrato_inicio_proceso_inicio';
+        if($this->security->isGranted('ROLE_UNABAST')) {
+            $url = 'minsal_contrato_inicio_proceso_inicio';
+        }
+        elseif ($this->security->isGranted('ROLE_UACI')){
+        	
+        }
+        elseif ($this->security->isGranted('ROLE_UFI')) {
+        	$url = 'minsal_contrato_listado_incrementos';
+        }
+
+        $response = new RedirectResponse($this->router->generate($url));
+            
+        return $response;
+    }
 }
