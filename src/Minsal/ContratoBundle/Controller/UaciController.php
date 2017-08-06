d6e4a898a11eeb1bb47983238284d00d20b7d6b5
Douglas Serbino <douglas@serbino>
2017-08-06 17:48:38 -0600

    Check List Updated

--- src/Minsal/ContratoBundle/Controller/UaciController.php
+++ src/Minsal/ContratoBundle/Controller/UaciController.php
@@ -38,7 +38,7 @@ class UaciController extends Controller
 		 INNER JOIN ctl_producto AS PR ON PR.id_producto_sibasi = PC.mtn_producto WHERE CM.id=?*/
 		$em = $this->getDoctrine()->getManager();
 
-		$dql = "SELECT DISTINCT c.id,pr.idProveedorSinab, pr.nit, pr.nombreProveedor, c.numeroContrato
+		$dql = "SELECT DISTINCT c.id,pr.idProveedorSinab, pr.nit, pr.nombreProveedor, c.numeroContrato, pr.estadoProveedor
 		FROM MinsalModeloBundle:CtlContrato c
 		INNER JOIN MinsalModeloBundle:MtnProductoContrato pc WITH c.idContratoSinab = pc.mtnContrato
 		INNER JOIN MinsalModeloBundle:CtlProveedor pr WITH c.contratoProveedor = pr.idProveedorSinab
