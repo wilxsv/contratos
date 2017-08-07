06d195588423480a5fb05a87104f1baf89de1c1a
Douglas Serbino <douglas@serbino>
2017-08-06 23:32:58 -0600

    Consulta

--- src/Minsal/ContratoBundle/Controller/AnalizadorController.php
+++ src/Minsal/ContratoBundle/Controller/AnalizadorController.php
@@ -46,56 +46,68 @@ class AnalizadorController extends Controller
 
 		public function listadoAction($incremento)
 		{
+			/*SELECT DISTINCT mc.id, mc.numero_modalidad, c.id_contrato_sinab, c.numero_contrato, pr.codigo_proveedor,
+			  pr.nombre_proveedor, p.codigo_producto, p.nombre_producto,c.monto_contrato,pc.cantidad,pc.precio_unitario,p.estado_producto, p.id_producto_sibasi
+			FROM ctl_producto AS p 
+			INNER JOIN mtn_producto_contrato AS pc ON p.id_producto_sibasi = pc.mtn_producto
+			INNER JOIN ctl_contrato AS c ON c.id_contrato_sinab = pc.mtn_contrato
+			INNER JOIN ctl_proveedor AS pr ON pr.id_proveedor_sinab = pc.mtn_proveedor
+			INNER JOIN ctl_modalidad_compra AS mc ON mc.id=c.numero_modalidad_compra 
+			WHERE mc.id = 1 AND pr.estado_proveedor = 4 AND p.estado_producto = 9
+			ORDER BY c.numero_contrato*/
+
 			$em = $this->getDoctrine()->getManager();
 			$incrementoObj = $em->getRepository('MinsalModeloBundle:CtlIncremento')->findOneBy(array(
 				'id'=>$incremento
 				));
-			$dql = "SELECT DISTINCT mc.id,mc.numeroModalidad,c.idContratoSibasi,c.numeroContrato,pr.codigoProveedor,pr.nombreProveedor,p.codigoProducto,p.nombreProducto,c.montoContrato,pc.cantidad,pc.precioUnitario,p.estadoProducto, p.idProductoSibasi
-			    FROM MinsalModeloBundle:CtlProducto p
-				INNER JOIN MinsalModeloBundle:MtnProductoContrato pc WITH p.idProductoSibasi=pc.mtnProducto 
-				INNER JOIN MinsalModeloBundle:CtlContrato c WITH pc.mtnContrato = c.idContratoSibasi 
-				INNER JOIN MinsalModeloBundle:CtlProveedor pr WITH pr.idProveedorSibasi = pc.mtnProveedor 
-				INNER JOIN MinsalModeloBundle:CtlModalidadCompra mc WITH mc.id=c.numeroModalidadCompra WHERE mc.id = $incremento AND pr.estadoProveedor=4 AND p.estadoProducto=9
-				ORDER BY c.numeroContrato";
-				$listado = $em->createQuery($dql)->getResult();
-				/* ahora es el analisis de cobertura */
-				$dataCobertura = array();
-				$estimacion = $incrementoObj->getEstimacion()->getId();
-				$mesesdesestimar = $incrementoObj->getMesesDesestimar();
-				$mesesdesestimar = $mesesdesestimar+3;
-				$fecha = date('Y-m-d');
-				$nuevafecha = strtotime ( "+$mesesdesestimar month" , strtotime ( $fecha ) ) ;
-				$nuevafecha = date ( 'Y-m-d' , $nuevafecha );
-				foreach ($listado as $medicamento){
-					// $service_url = "http://192.168.1.13:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&idproducto={$medicamento["idProductoSibasi"]}&programacion={$estimacion}";
-					// esta solo es para pruebas
-					$service_url = "http://192.168.1.13:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&idproducto={$medicamento["idProductoSibasi"]}&programacion=1";
-				      $curl = curl_init($service_url);
-				      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-				      $curl_response = curl_exec($curl);
-				      curl_close($curl);
-				      $respuesta = json_decode($curl_response,true);
+		 	$dql = "SELECT DISTINCT mc.id, mc.numeroModalidad, c.idContratoSinab, c.numeroContrato, pr.codigoProveedor, pr.nombreProveedor, p.codigoProveedor, p.nombreProducto, c.montoContrato, pc.cantidad, pc.precioUnitario, p.estadoProducto, p.idProductoSibasi
+			    	FROM MinsalModeloBundle:CtlProducto p 
+					INNER JOIN MinsalModeloBundle:MtnProductoContrato pc WITH  p.idProductoSibasi = pc.mtnProducto
+					INNER JOIN MinsalModeloBundle:CtlContrato c WITH  c.idContratoSinab = pc.mtnContrato
+					INNER JOIN MinsalModeloBundle:CtlProducto pr WITH  pr.idProveedorSinab = pc.mtnProveedor
+					INNER JOIN MinsalModeloBundle:CtlModalidadCompra mc WITH mc.id=c.numeroModalidadCompra
+					WHERE mc.id = $incremento AND pr.estadoProveedor = 4 AND p.estadoProducto = 9 
+					ORDER BY c.numeroContrato ";
+					
+			$listado = $em->createQuery($dql)->getResult();
+			/* ahora es el analisis de cobertura */
+			$dataCobertura = array();
+			$estimacion = $incrementoObj->getEstimacion()->getId();
+			$mesesdesestimar = $incrementoObj->getMesesDesestimar();
+			$mesesdesestimar = $mesesdesestimar+3;
+			$fecha = date('Y-m-d');
+			$nuevafecha = strtotime ( "+$mesesdesestimar month" , strtotime ( $fecha ) ) ;
+			$nuevafecha = date ( 'Y-m-d' , $nuevafecha );
+			foreach ($listado as $medicamento){
+				// $service_url = "http://192.168.1.13:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&idproducto={$medicamento["idProductoSibasi"]}&programacion={$estimacion}";
+				// esta solo es para pruebas
+				$service_url = "http://192.168.1.13:8080/v1/sinab/datoscobertura?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&idproducto={$medicamento["idProductoSibasi"]}&programacion=1";
+			      $curl = curl_init($service_url);
+			      curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			      $curl_response = curl_exec($curl);
+			      curl_close($curl);
+			      $respuesta = json_decode($curl_response,true);
 
-				      $service_url2 = "http://192.168.1.13:8080/v1/sinab/existencianacional?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&producto={$medicamento["idProductoSibasi"]}&fecha={$nuevafecha}";
-				      $curl2 = curl_init($service_url2);
-				      curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
-				      $curl_response2 = curl_exec($curl2);
-				      curl_close($curl2);
-				      $respuesta2 = json_decode($curl_response2,true);
+			      $service_url2 = "http://192.168.1.13:8080/v1/sinab/existencianacional?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&producto={$medicamento["idProductoSibasi"]}&fecha={$nuevafecha}";
+			      $curl2 = curl_init($service_url2);
+			      curl_setopt($curl2, CURLOPT_RETURNTRANSFER, true);
+			      $curl_response2 = curl_exec($curl2);
+			      curl_close($curl2);
+			      $respuesta2 = json_decode($curl_response2,true);
 
-				      foreach ($respuesta['respuesta'] as $med ) {
-				      	foreach ($respuesta2['respuesta'] as $exis) {
-				      		array_push($med, $exis["0"]);
-				      		array_push($dataCobertura, $med);
-				      	}
-				      	
-				      }
-				}
-				
-				unset($dataCobertura["0"]);
-				return $this->render('MinsalPlantillaBundle:Analizador:contratos.html.twig',array(
-	     	'listado'=>$listado,'dataCobertura'=>$dataCobertura 
-	     	));
+			      foreach ($respuesta['respuesta'] as $med ) {
+			      	foreach ($respuesta2['respuesta'] as $exis) {
+			      		array_push($med, $exis["0"]);
+			      		array_push($dataCobertura, $med);
+			      	}
+			      	
+			      }
+			}
+			
+			unset($dataCobertura["0"]);
+			return $this->render('MinsalPlantillaBundle:Analizador:contratos.html.twig',array(
+     		'listado'=>$listado,'dataCobertura'=>$dataCobertura 
+     	));
 		}
 
 }
