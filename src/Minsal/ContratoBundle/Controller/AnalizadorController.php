742a7e4a2b55701521b73a0b1d148f2a04d375d8
Douglas Serbino <douglas@serbino>
2017-08-13 01:12:34 -0600

    Cambios

--- src/Minsal/ContratoBundle/Controller/AnalizadorController.php
+++ src/Minsal/ContratoBundle/Controller/AnalizadorController.php
@@ -28,39 +28,51 @@ class AnalizadorController extends Controller
 	 			$establecimiento = $_GET["establecimiento"];
 	 			$proveedor = $_GET["proveedor"];
 	 		*/
-
 			$em = $this->getDoctrine()->getManager(); //Invocamos al manejador de Entidades
 			$prorrogaObj = $em->getRepository('MinsalModeloBundle:CtlProrroga')->findOneBy(array(
 				'id'=>$prorroga
 				)); //Obtenemos el ID de la prorroga al que se analizara
 			$compra = $prorrogaObj->getProrrogaModalidadCompra()->getId(); //Obtenemos el ID De la modalidad de la compra
+			$dql = "SELECT mi.medicamentos FROM MinsalModeloBundle:CtlIncremento i INNER JOIN MinsalModeloBundle:MtnMedicamentoIncremento mi WITH i.id=mi.incrementoid WHERE i.id=$prorroga";
 
-			$licitacion = $prorrogaObj->getProrrogaModalidadCompra()->getNumeroModalidad(); //Obtenemos el codigo de licitacion a utilizar
+			$listado = $em->createQuery($dql)->getResult();
 
-			$dql = "SELECT mc.id, mc.numeroModalidad, c.id as contrato, c.numeroContrato, p.codigoProducto, p.nombreProducto, c.montoContrato, pc.cantidad, pc.precioUnitario, p.id, pr.nombreProveedor, IDENTITY (p.establecimientoProducto) AS establecimientoProductoP, pr.id AS idProveedor
+			$listadounido = '';
+			foreach ($listado as $lista) {
+				$listadounido .=$lista["medicamentos"];
+			}
+			$listadounido = str_replace('[','', $listadounido);
+			$listadounido = str_replace(']','', $listadounido);
+			$listadounido = str_replace('"','', $listadounido);
+
+			$licitacion = $prorrogaObj->getProrrogaModalidadCompra()->getNumeroModalidad(); 
+
+			$dql2 = "SELECT mc.id, mc.numeroModalidad, c.id as contrato, c.numeroContrato, p.codigoProducto, p.nombreProducto, c.montoContrato, pc.cantidad, pc.precioUnitario, p.id, pr.nombreProveedor, IDENTITY (p.establecimientoProducto) AS establecimientoProductoP, pr.id AS idProveedor
 			    	FROM MinsalModeloBundle:CtlProducto p 
 					INNER JOIN MinsalModeloBundle:MtnProductoContrato pc WITH pc.mtnProducto = p.id  
 					INNER JOIN MinsalModeloBundle:CtlContrato c WITH  pc.mtnContrato = c.id 
 					INNER JOIN MinsalModeloBundle:CtlProveedor pr WITH pc.mtnProveedor = pr.id
 					INNER JOIN MinsalModeloBundle:CtlModalidadCompra mc WITH c.numeroModalidad = mc.id 
-					WHERE mc.id = $compra AND pr.estadoProveedor = 4 AND p.estadoProducto = 9
-					ORDER BY c.numeroContrato ";
-			$listado = $em->createQuery($dql)->getResult();
+					WHERE p.estadoProducto = 9 AND p.id IN ($listadounido) AND mc.id = $compra
+					ORDER BY c.id ";
 
+			$medicamentoslista = $em->createQuery($dql2)->getResult();
+			$listadep = array();
+			foreach ($medicamentoslista as $m) {
+ 				array_push($listadep, $m["id"]);
+			}
+			$listadepunido = implode(",", $listadep);
 
 			$planificacion = $prorrogaObj->getPlanificacion()->getId(); //Obtenermos el ID de la programacion que se utilizara
 
-			foreach ($listado as $medicamento) {
-				$establecimiento = $medicamento['establecimientoProductoP'];
-				$proveedor = $medicamento['idProveedor'];
-				$service_url = "http://192.168.1.14:8080/v1/sinab/medicamentosplanificacionprorroga?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&programacion=1&licitacion=06/2016&establecimiento=621&proveedor=5";
+			/*http://192.168.1.14:8080/v1/sinab/medicamentosplanificacionprorroga?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&programacion=1&licitacion=06/2016&establecimiento=621&proveedor=5*/
+			$service_url = "http://192.168.1.14:8080/v1/sinab/medicamentosplanificacionprorroga?tocken=eccbc87e4b5ce2fe28308fd9f2a7baf3&programacion=1&licitacion=06/2016&establecimiento=621&proveedor=5";
 
-				$curl = curl_init($service_url);
-			    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-			    $curl_response = curl_exec($curl);
-			    curl_close($curl);
-			    $respuesta = json_decode($curl_response,true);
-			}
+			$curl = curl_init($service_url);
+      		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+      		$curl_response = curl_exec($curl);
+      		curl_close($curl);
+      		$respuesta = json_decode($curl_response,true);
 
 			return $this->render('MinsalPlantillaBundle:Analizador:prorroga.html.twig', array(
 				'servicio'=>$service_url,
