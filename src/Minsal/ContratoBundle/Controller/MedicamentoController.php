a74899520e2a9038d980f80d010071d556fce601
Douglas Serbino <douglas@serbino>
2017-08-14 02:48:06 -0600

    PLUS INCREMENTO

--- src/Minsal/ContratoBundle/Controller/MedicamentoController.php
+++ src/Minsal/ContratoBundle/Controller/MedicamentoController.php
@@ -92,6 +92,7 @@ class MedicamentoController extends Controller
 		    $error = 'full';
 		    return $this->render('MinsalPlantillaBundle:Producto:depuracion.html.twig',array(
 		    	'medicamentos' => $medicamentos,
+		    	'incrementop' => $incremento,
 		    	'error'=>$error));
 		}
 	   
